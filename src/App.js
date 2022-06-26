e50d5672b68e04c5020839538a19d99d74b168c9
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2022-06-26 16:27:10 +0100

    Added Chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import Parse from 'parse';
+import { useEffect, useState } from 'react';
+import { initializeParse } from '@parse/react';
+
+const PARSE_APPLICATION_ID = 'mobi';
+const PARSE_JAVASCRIPT_KEY = 'mobi-JSKEY';
+const PARSE_HOST_URL = 'http://pulsecheck.9ijakids.com/parse';
+
+// import axios from 'axios';
+// import { useForm } from 'react-hook-form';
+
+// const PARSE_APPLICATION_ID = 'mobi';
+// const PARSE_HOST_URL = 'https://pulsecheck.9ijakids.com';
+// const PARSE_JAVASCRIPT_KEY = 'mobi-JSKEY';
+// Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+// Parse.serverURL = PARSE_HOST_URL;
+initializeParse(PARSE_HOST_URL, PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.liveQueryServerURL = 'wss://pulsecheck.9ijakids.com/parse';
+// Parse.enableLocalDatastore();
+
+// var client = new Parse.LiveQueryClient({
+// 	applicationId: PARSE_APPLICATION_ID,
+// 	serverURL: 'wss://pulsecheck.9ijakids.com/parse', // Example: 'wss://livequerytutorial.back4app.io'
+// 	javascriptKey: PARSE_JAVASCRIPT_KEY,
+// });
+// client.open();
+
+// client.on('connection', () => {
+// 	console.log('connected');
+// });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [sub, setSub] = useState(null);
+
+	useEffect(() => {
+		async function setSubscription() {
+			let query = new Parse.Query('Messages');
+
+			try {
+				let subscription = await query.subscribe();
+
+				subscription.on('open', () => {
+					console.log('subscription opened');
+				});
+
+				subscription.on('create', (data) => {
+					console.log(data);
+				});
+				setSub(subscription);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+
+		setSubscription()
+	}, []);
+
+	// useEffect(() => {}, [sub]);
+
+	return (
+		<div className="App">
+			<h1>Parse Testing</h1>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import Parse minified version
+// import Parse from 'parse/dist/parse.min.js';
+
+// // Your Parse initialization configuration goes here
+// const PARSE_APPLICATION_ID = 'mobi';
+// const PARSE_HOST_URL = 'http://pulsecheck.9ijakids.com/';
+// const PARSE_JAVASCRIPT_KEY = 'mobi-JSKEY';
+// Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+// Parse.enableLocalDatastore();
+// Parse.serverURL = PARSE_HOST_URL;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
